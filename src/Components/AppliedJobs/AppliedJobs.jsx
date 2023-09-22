75ce7ba76285f62e16792cb31e06ef5b31f575ac
omarjafor <omarjaforchy@gmail.com>
2023-09-22 11:41:38 +0600

    react router career hub

---
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -0,0 +1,9 @@
+const Appliedjobs = () => {
+    return (
+        <div>
+            <h1>Jobs I Applied is Here</h1>
+        </div>
+    );
+};
+
+export default Appliedjobs;
--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,22 @@ import './App.css'
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import Root from './Components/Root/Root';
 import Home from './Components/Home/Home';
+import Appliedjobs from './Components/AppliedJobs/Appliedjobs';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: '/',
         element: <Home></Home>
+      },
+      {
+        path: '/appliedjobs',
+        element: <Appliedjobs></Appliedjobs>
       }
     ]
   },
