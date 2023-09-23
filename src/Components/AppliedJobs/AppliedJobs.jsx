a50b1da16390110b42afc4873f06521f3e7a65d1
omarjafor <omarjaforchy@gmail.com>
2023-09-23 13:27:36 +0600

    applyed job eslint disable the react

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -4,17 +4,37 @@ import { getStoredJobApplication } from "../../utility/localstorage";
 
 const Appliedjobs = () => {
     const jobs = useLoaderData();
+    const [appliedJobs, setAppliedJobs] = ([]);
+    const [displayJobs, setDisplayJobs] = ([]);
+
+    
     useEffect( ()=>{
         const storedJobIds = getStoredJobApplication();
         if(jobs.length > 0){
             const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id))
-            console.log(jobs, storedJobIds, jobsApplied);
+            setAppliedJobs(jobsApplied);
+            setDisplayJobs(jobsApplied);
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     return (
         <div>
-            <h1 className="text-2xl font-bold">Jobs I Applied : </h1>
+            <h2 className="text-2xl">Jobs I applied: {appliedJobs.length}</h2>
+            <details className="dropdown mb-32">
+                <summary className="m-1 btn">open or close</summary>
+                <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                    <li onClick={() => handleJobsFilter('all')}><a>All</a></li>
+                    <li onClick={() => handleJobsFilter('remote')}><a>Remote</a></li>
+                    <li onClick={() => handleJobsFilter('onsite')}><a>onsite</a></li>
+                </ul>
+            </details>
+            <ul>
+                {
+                    displayJobs.map(job => <li key={job.id}>
+                        <span>{job.job_title} {job.company_name}: {job.remote_or_onsite} </span>
+                    </li>)
+                }
+            </ul>
         </div>
     );
 };
